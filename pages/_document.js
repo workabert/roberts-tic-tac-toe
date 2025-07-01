5207cacff952c6ca0a686011c7f2d44be1b44f5d
workabert <21-69059@g.batstate-u.edu.ph>
2025-07-02 01:14:10 +0800

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Add Tailwind CDN */}
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
