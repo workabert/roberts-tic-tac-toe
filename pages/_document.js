5829943e69dee916854681aed1abee448e02b278
workabert <21-69059@g.batstate-u.edu.ph>
2025-07-02 02:04:41 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,14 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-        {/* Add Tailwind CDN */}
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
